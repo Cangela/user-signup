41c49ef84b694d7d90108eae894db6381507dc3f
Angela Tibbs <adtibbs@gmail.com>
2017-01-30 08:05:00 -0600

    added buttons for username, password, verify password, e-mail

--- main.py
+++ main.py
@@ -15,10 +15,61 @@
 # limitations under the License.
 #
 import webapp2
+import cgi
+import re
+
+
+form= """
+<h1>Signup</h1>
+
+
+<form  ="post">
+    <table>
+         <tr>
+            <td><label for="username">Username</label></td>
+            <td>
+            <input  type="text" name = "username" ><br>
+            </td>
+        </tr>
+        <tr>
+            <td><label for="password">Password</label></td>
+            <td>
+            <input type="password" name = "password"><br>
+            </td>
+        </tr>
+        <tr>
+            <td><label for="verify password">Verify Password</label></td>
+            <td>
+            <input type="password" name = "verify"><br>
+            </td>
+        </tr>
+        <tr>
+            <td><label for="email">Email (otional)</label></td>
+            <td>
+            <input name = "email"><br>
+            </td>
+        </tr>
+    </table>
+    <input type="submit">
+
+</form>
+"""
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(form)
+        username = self.request.get('username')
+        password = self.request.get('password')
+        verify = self.request.get('verify')
+
+    def post(self):
+        self.response.write(form)
+        if len('username') <3:
+            self.response.write("No!")
+        else:
+            self.response.write("hello + username")
+#class WelcomePage(object):
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
